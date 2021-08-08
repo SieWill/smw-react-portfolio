40b10db121a4eb6e093ab675cc7c44bdc50efab7
SieBeCoding <siebecoding@gmail.com>
2021-08-08 17:48:17 -0400

    New and Edit workflows

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -136,7 +136,11 @@ export default class PortfolioForm extends Component {
       withCredentials: true,
     })
       .then((response) => {
-        this.props.handleSuccessfulFormSubmission(response.data.portfolio_item);
+        if (this.state.editMode) {
+          this.props.handleEditFormSubmission();
+        } else {
+          this.props.handleNewFormSubmission(response.data.portfolio_item);
+        }
 
         this.setState({
           name: "",
@@ -147,6 +151,9 @@ export default class PortfolioForm extends Component {
           thumb_image: "",
           banner_image: "",
           logo: "",
+          editMode: false,
+          apiUrl: `https://sierrawilliams.devcamp.space/portfolio/portfolio_items/${id}`,
+          apiAction: "patch",
         });
 
         [this.thumbRef, this.bannerRef, this.logoRef].forEach((ref) => {
