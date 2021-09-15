788533fdb884e6572da1745758835f996226ae25
SieBeCoding <siebecoding@gmail.com>
2021-09-15 19:58:38 -0400

    added content to Draft JS

--- src/components/blog/blog-form.js
+++ src/components/blog/blog-form.js
@@ -139,12 +139,12 @@ export default class BlogForm extends Component {
         <div className="one-column">
           <RichTextEditor
             handleRichTextEditorChange={this.handleRichTextEditorChange}
-            // editMode={this.props.editMode}
-            // contentToEdit={
-            //   this.props.editMode && this.props.blog.content
-            //      this.props.blog.content
-            //      : null
-            // }
+            editMode={this.props.editMode}
+            contentToEdit={
+              this.props.editMode && this.props.blog.content
+                ? this.props.blog.content
+                : null
+            }
           />
         </div>
         <div className="image-uploaders">
